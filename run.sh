2056eb957905d735928cf7ed20e9e155f138ee9c
Rabenknecht <rabenknecht@proton.me>
2026-03-18 13:40:00 +0100

    New run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+# I run the server, clearing storage and using the questions of the repository
+
+cd src
+dotnet build
+cd ..
+dotnet src/bin/Debug/net10.0/the_assembly_server.dll -q questions/uncategorized.txt -c -ne
